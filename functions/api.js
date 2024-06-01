8efdcef6e6bc6beaf1d095d41054b701e2540d8a
KenntenMontero <kennethmontero46@gmail.com>
2024-06-01 09:54:56 +0800

    api done

---
+++ functions/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/orderRoutes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const MongoDbCloud = 'mongodb+srv://vigocharlesmc:qIwYJ5V0lpKVutlz@cluster0.5esntvv.mongodb.net/Order?retryWrites=true&w=majority&appName=Cluster0';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose
+  .connect(MongoDbCloud)
+  .then(()=> console.log('Connected to MongoDB'))
+  .catch((err)=>console.error('Failed to connect to MongoDB'));
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
